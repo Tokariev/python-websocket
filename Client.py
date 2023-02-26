8aad5b366806e5c6c275ff7ca646abc635b71e0b
Mykola Tokariev <mykola.tokariev@gmail.com>
2023-02-26 12:34:15 +0100

    Add response to client

--- Client.py
+++ Client.py
@@ -1,12 +1,12 @@
 import socket
 
 
-SERVER_ADDRESS = ('127.0.0.1', 5001)
+SERVER_ADDRESS = ('127.0.0.1', 5002)
 
-client_socket = socket.socket(socket.AF_INET,     #Internet Protocol
-                              socket.SOCK_STREAM) #TCP Protocol
+client_socket = socket.socket(socket.AF_INET,
+                              socket.SOCK_STREAM)
 
-
+print('I am Client\n')
 client_socket.connect(SERVER_ADDRESS)
 client_socket.send(bytes('Hello Server', 'utf-8'))
 
--- Server.py
+++ Server.py
@@ -1,17 +1,22 @@
 import socket
+import time
 
 
-SERVER_ADDRESS = ('127.0.0.1', 5001)
+SERVER_ADDRESS = ('127.0.0.1', 5002)
 
 server_socket = socket.socket(socket.AF_INET,
                               socket.SOCK_STREAM)
 
+print('I am Server\n')
 server_socket.bind(SERVER_ADDRESS)
 server_socket.listen(5)
-print('Server is listening...')
 
 while True:
     (client_socket, address) = server_socket.accept()
     msg = client_socket.recv(1024)
     print(msg.decode('utf-8'))
-    client_socket.send(bytes('Hello Client', 'utf-8'))
+    client_socket.send(bytes('Hello Client\n', 'utf-8'))
+
+    for i in range(10):
+        client_socket.send(bytes(str(i), 'utf-8'))
+        time.sleep(1)
